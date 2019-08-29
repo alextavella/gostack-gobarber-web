f64e153d5753fec38ff26b6a922e1b42c59db9af
Alex Tavella <alextavella@outlook.com>
2019-08-29 18:04:08 -0300

    Add failure auth

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -8,18 +8,23 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case authActions.SIGN_IN_REQUEST:
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case authActions.SIGN_IN_REQUEST: {
         draft.loading = true;
-      });
-    case authActions.SIGN_IN_SUCCESS:
-      return produce(state, draft => {
+        break;
+      }
+      case authActions.SIGN_IN_SUCCESS: {
         draft.token = action.payload.token;
         draft.signed = true;
         draft.loading = false;
-      });
-    default:
-      return state;
-  }
+        break;
+      }
+      case authActions.SIGN_FAILURE: {
+        draft.loading = false;
+        break;
+      }
+      default:
+    }
+  });
 }
